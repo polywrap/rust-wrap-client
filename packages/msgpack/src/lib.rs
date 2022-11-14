df853a9fbd17251fb7139c464b4b78f04eaeaf19
Cesar <cesarbrazon10@gmail.com>
2022-11-14 15:52:57 +0100

    test(msgpack): decode tests created

--- packages/msgpack/src/lib.rs
+++ packages/msgpack/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 pub use rmpv::{encode::write_value, Value};
 pub use rmp_serde::{Serializer};
 pub use serde::{Serialize, de::DeserializeOwned, Deserialize};
@@ -38,3 +40,29 @@ impl RMPVObject {
 }
 
 pub mod macros;
+
+#[derive(Deserialize,Debug,PartialEq)]
+struct ValueTest {
+    pub code: i32,
+    pub success: bool,
+    pub payload: HashMap<String, Vec<String>>
+}
+
+#[test]
+fn vec_to_struct() {
+    let encoded_struct: [u8; 50] = [
+        131, 164, 99, 111, 100, 101, 204, 200, 167, 115, 117, 99, 99, 101, 115, 115, 195,
+        167, 112, 97, 121, 108, 111, 97, 100, 129, 168, 102, 101, 97, 116, 117, 114, 101,
+        115, 146, 165, 115, 101, 114, 100, 101, 167, 109, 115, 103, 112, 97, 99, 107
+    ];
+
+    let response = decode::<ValueTest>(&encoded_struct);
+    let mut payload = HashMap::new();
+    payload.insert("features".to_string(), vec!["serde".to_string(), "msgpack".to_string()]);
+    let expected = ValueTest {
+        code: 200,
+        success: true,
+        payload
+    };
+    assert_eq!(response.unwrap(), expected);
+}
