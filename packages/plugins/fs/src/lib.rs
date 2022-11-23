c8235b1d76f80231ed368c24784d940d27a6dbbe
namesty <nestor09amesty@gmail.com>
2022-11-23 13:29:25 +0100

    (feat): fs plugin implemented

---
+++ packages/plugins/fs/src/lib.rs
@@ -0,0 +1,89 @@
+use std::{sync::Arc, fs, path::Path};
+
+use async_trait::async_trait;
+use polywrap_core::{invoke::Invoker, error::Error};
+use polywrap_plugin::{module::PluginModule, package::PluginPackage, wrapper::PluginWrapper};
+use tokio::sync::Mutex;
+use wrap::{module::{Module, ArgsReadFile, ArgsReadFileAsString, ArgsExists, ArgsWriteFile, ArgsMkdir, ArgsRm, ArgsRmdir}, wrap_info::get_manifest};
+pub mod wrap;
+
+pub struct FileSystemPlugin {}
+
+#[async_trait]
+impl Module for FileSystemPlugin {
+  async fn read_file(&mut self, args: &ArgsReadFile, _: Arc<dyn Invoker>) -> Result<Vec<u8>, Error> {
+    fs::read(&args.path).map_err(|e| Error::InvokeError(e.to_string()))
+  }
+
+  async fn read_file_as_string(&mut self, args: &ArgsReadFileAsString, _: Arc<dyn Invoker>) -> Result<String, Error> {
+    fs::read_to_string(&args.path).map_err(|e| Error::InvokeError(e.to_string()))
+  }
+
+  async fn exists(&mut self, args: &ArgsExists, _: Arc<dyn Invoker>) -> Result<bool, Error> {
+    Ok(Path::new(&args.path).exists())
+  }
+
+  async fn write_file(&mut self, args: &ArgsWriteFile, _: Arc<dyn Invoker>) -> Result<Option<bool>, Error> {
+    fs::write(Path::new(&args.path), String::from_utf8(args.data.clone()).unwrap()).unwrap();
+
+    Ok(Some(true))
+  }
+
+  async fn mkdir(&mut self, args: &ArgsMkdir, _: Arc<dyn Invoker>) -> Result<Option<bool>, Error> {
+    let recursive = if let Some(recursive) = args.recursive {
+      recursive
+    } else {
+      false
+    };
+
+    let path = Path::new(&args.path);
+
+    if recursive {
+      fs::create_dir_all(path).unwrap();
+    } else {
+      fs::create_dir(path).unwrap();
+    }
+    
+    Ok(Some(true))
+  }
+
+  async fn rm(&mut self, args: &ArgsRm, _: Arc<dyn Invoker>) -> Result<Option<bool>, Error> {
+    let recursive = if let Some(recursive) = args.recursive {
+      recursive
+    } else {
+      false
+    };
+
+    let force = if let Some(force) = args.force {
+      force
+    } else {
+      false
+    };
+
+    let path = Path::new(&args.path);
+    
+    if path.is_dir() {
+      if force {
+        rm_rf::ensure_removed(path).unwrap();
+      } else {
+        if recursive {
+          fs::remove_dir_all(path).unwrap();
+        } else {
+          fs::remove_dir(path).unwrap();
+        }
+      }
+    } else {
+      fs::remove_file(path).unwrap();
+    }
+
+    Ok(Some(true))
+  }
+
+  async fn rmdir(&mut self, args: &ArgsRmdir, _: Arc<dyn Invoker>) -> Result<Option<bool>, Error>{
+    fs::remove_dir(&args.path).unwrap();
+
+    Ok(Some(true))
+  }
+}
+
+impl_traits!(FileSystemPlugin);
--- packages/plugins/http-resolver/src/lib.rs
+++ packages/plugins/http-resolver/src/lib.rs
@@ -1,1 +1,103 @@
+use std::sync::Arc;
+
+use crate::wrap::types::Http_Module_ArgsGet;
+use async_trait::async_trait;
+use polywrap_core::{error::Error, invoke::Invoker};
+use wrap::{
+    module::{ArgsGetFile, ArgsTryResolveUri, Module},
+    types::{HttpModule, HttpRequest, MaybeUriOrManifest},
+};
 pub mod wrap;
+
+pub struct HttpResolverPlugin {}
+
+#[async_trait]
+impl Module for HttpResolverPlugin {
+    async fn try_resolve_uri(
+        &mut self,
+        args: &ArgsTryResolveUri,
+        invoker: Arc<dyn Invoker>,
+    ) -> Result<Option<MaybeUriOrManifest>, Error> {
+        if args.authority != "http" && args.authority != "https" {
+            return Ok(None);
+        };
+
+        let manifest_search_pattern = "wrap.info";
+
+        let get_result = HttpModule::get(
+            &Http_Module_ArgsGet {
+                url: format!("{}/{}", args.path, manifest_search_pattern),
+                request: Some(HttpRequest {
+                    response_type: wrap::types::HttpResponseType::BINARY,
+                    headers: None,
+                    body: None,
+                    url_params: None,
+                }),
+            },
+            invoker,
+        )
+        .await;
+
+        let manifest = match get_result {
+            Ok(opt_response) => {
+                if let Some(response) = opt_response {
+                    if let Some(body) = response.body {
+                        Some(base64::decode(body).unwrap())
+                    } else {
+                        None
+                    }
+                } else {
+                    None
+                }
+            }
+            Err(_) => {
+                // TODO: logging
+                // https://github.com/polywrap/monorepo/issues/33
+                None
+            }
+        };
+
+        Ok(Some(MaybeUriOrManifest {
+            uri: None,
+            manifest,
+        }))
+    }
+
+    async fn get_file(
+        &mut self,
+        args: &ArgsGetFile,
+        invoker: Arc<dyn Invoker>,
+    ) -> Result<Option<Vec<u8>>, Error> {
+        let resolve_result = HttpModule::get(
+            &Http_Module_ArgsGet {
+                url: args.path,
+                request: Some(HttpRequest {
+                    response_type: wrap::types::HttpResponseType::BINARY,
+                    headers: None,
+                    body: None,
+                    url_params: None,
+                }),
+            },
+            invoker,
+        )
+        .await;
+
+        let file = if let Ok(opt_result) = resolve_result {
+            if let Some(result) = opt_result {
+                if let Some(body) = result.body {
+                    Some(base64::decode(body).unwrap())
+                } else {
+                    None
+                }
+            } else {
+                None
+            }
+        } else {
+            None
+        };
+
+        Ok(file)
+    }
+}
+
+impl_traits!(HttpResolverPlugin);
