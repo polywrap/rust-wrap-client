d9b32056c64519fdc423f031a8e592470be6c086
Cesar <cesarbrazon10@gmail.com>
2023-01-25 21:54:37 +0100

    chore: add mock package

--- packages/tests-utils/src/helpers.rs
+++ packages/tests-utils/src/helpers.rs
@@ -1,9 +1,12 @@
-use std::{path::{Path,PathBuf},sync::Arc, fmt::{Debug, Formatter}};
+use std::{path::{Path,PathBuf},sync::{Arc}, fmt::{Debug, Formatter}};
 use async_trait::async_trait;
+use futures::lock::Mutex;
 
-use polywrap_core::{wrapper::{Wrapper, GetFileOptions},  invoke::Invoker, uri::Uri, env::Env, resolvers::uri_resolution_context::UriResolutionContext};
+use polywrap_core::{wrapper::{Wrapper, GetFileOptions},  invoke::Invoker, uri::Uri, env::Env, resolvers::uri_resolution_context::UriResolutionContext, package::WrapPackage};
+use wrap_manifest_schemas::versions::WrapManifest;
 
 pub struct MockWrapper;
+pub struct MockPackage;
 
 impl Debug for MockWrapper {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
@@ -14,6 +17,33 @@ impl Debug for MockWrapper {
 impl MockWrapper {
     pub fn new() -> Self {
         Self {}
+    }
+}
+
+
+impl Debug for MockPackage {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "MockPackage")
+    }
+}
+
+impl MockPackage {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+#[async_trait]
+impl WrapPackage for MockPackage {
+    async fn create_wrapper(&self) -> Result<Arc<Mutex<dyn Wrapper>>, polywrap_core::error::Error> {
+        Ok(Arc::new(Mutex::new(get_mock_wrapper())))
+    }
+
+    async fn get_manifest(
+        &self, 
+        _: Option<polywrap_core::package::GetManifestOptions>
+    ) ->  Result<WrapManifest, polywrap_core::error::Error> {
+        unimplemented!()
     }
 }
 
