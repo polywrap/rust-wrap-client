64772b3d1eb8436f8a3ce1d03f3b8d36f6ef148a
namesty <nestor09amesty@gmail.com>
2023-03-31 16:27:17 +0200

    (chore): fixed msgpack serialization for generic map

--- packages/client/tests/test_map_type.rs
+++ packages/client/tests/test_map_type.rs
@@ -4,20 +4,21 @@ use polywrap_client::builder::types::{BuilderConfig, ClientBuilder, ClientConfig
 use polywrap_client::client::PolywrapClient;
 use polywrap_client::core::uri::Uri;
 use polywrap_client::msgpack::msgpack;
+use polywrap_msgpack::Value;
 use polywrap_msgpack::extensions::generic_map::GenericMap;
 use polywrap_tests_utils::helpers::get_tests_path;
 use serde::{Serialize, Deserialize};
 
 #[derive(Serialize,Deserialize)]
 pub struct ArgsGetKey {
+    pub foo: CustomMap,
     pub key: String,
-    pub foo: CustomMap
 }
 
 #[derive(Serialize,Deserialize)]
 pub struct CustomMap {
     pub map: GenericMap<String, u32>,
-    pub nested_map: GenericMap<String, GenericMap<String, u32>>
+    pub nestedMap: GenericMap<String, GenericMap<String, u32>>
 }
 
 
@@ -34,30 +35,27 @@ fn map_type_test() {
     let client = PolywrapClient::new(config);
 
     let mut myMap = GenericMap(BTreeMap::new());
-    myMap.0.insert(String::from("hello"), 1);
-    myMap.0.insert(String::from("heyo"), 50);
+    myMap.0.insert(String::from("Hello"), 1);
+    myMap.0.insert(String::from("Heyo"), 50);
 
 
     let mut myNestedMap = GenericMap(BTreeMap::new());
     let mut insideNestedMap = GenericMap(BTreeMap::new());
 
-    insideNestedMap.0.insert(String::from("hello"), 1);
-    insideNestedMap.0.insert(String::from("heyo"), 50);
-    myNestedMap.0.insert(String::from("nested"), insideNestedMap);
+    insideNestedMap.0.insert(String::from("Hello"), 1);
+    insideNestedMap.0.insert(String::from("Heyo"), 50);
+    myNestedMap.0.insert(String::from("Nested"), insideNestedMap);
     let foo = CustomMap {
         map: myMap,
-        nested_map: myNestedMap
+        nestedMap: myNestedMap
     };
 
     let getKeyArgs = ArgsGetKey {
-        foo: foo,
-        key: String::from("hello")
+      key: String::from("Hello"),
+      foo: foo,
     };
 
-    let args = rmp_serde::encode::to_vec(&getKeyArgs).unwrap();
-    println!("args: {:?}", args);
-    // let args = polywrap_msgpack::encode(&getKeyArgs).unwrap();
-
+    let args = polywrap_msgpack::serialize(&getKeyArgs).unwrap();
     let invoke_result = client
         .invoke::<u32>(
             &invoke_uri,
