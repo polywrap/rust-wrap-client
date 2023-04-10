e86cfb94bbdcf2f1c36316f249349f0568fb0d73
Cesar <cesarbrazon10@gmail.com>
2023-04-10 04:24:53 +0200

    chore: remove unneeded changes in plugin

--- packages/plugin/src/lib.rs
+++ packages/plugin/src/lib.rs
@@ -5,9 +5,3 @@ pub mod method;
 pub mod with_methods;
 pub mod error;
 pub mod utils;
-
-#[cfg(feature = "polywrap_plugin_creator")]
-pub use polywrap_plugin_creator::{
-    plugin_impl,
-    plugin_struct
-};
