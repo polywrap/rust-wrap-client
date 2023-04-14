dd695b9e1d432866cbf5035d315876c1f8f86444
namesty <nestor09amesty@gmail.com>
2023-04-14 19:39:37 +0200

    (fix): plugin env test

--- packages/plugin/tests/test_invocation.rs
+++ packages/plugin/tests/test_invocation.rs
@@ -27,13 +27,16 @@ pub trait Module: PluginModule {
 impl Module for PluginEnv {
     fn check_env_is_bar(
         &mut self,
-        _: &GetEnvArgs,
+        args: &GetEnvArgs,
         env: Option<Env>,
         _: Arc<dyn Invoker>
     ) -> Result<bool, PluginError> {
-        if let Some(e) = env {
-            return Ok(e.eq(&Value::String("bar".to_string())));
+        if let Some(env) = env {
+          if let Some(value) = env.get(args.key.clone()) {
+            return Ok(value.eq(&Value::String("bar".to_string())));
+          }
         }
+
         Ok(false)
     }
 }
