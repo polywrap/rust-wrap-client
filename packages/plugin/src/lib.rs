c43912c1c30b2ced59221486c739a9882ca352da
Cesar <cesarbrazon10@gmail.com>
2023-04-24 17:23:43 +0200

    chore(plugin): export crates necessary in codegen

--- packages/plugin/src/lib.rs
+++ packages/plugin/src/lib.rs
@@ -7,3 +7,9 @@ pub mod error;
 pub mod utils;
 
 pub use polywrap_plugin_implementor as implementor;
+
+pub use bigdecimal::BigDecimal as BigNumber;
+pub use num_bigint::BigInt;
+pub use serde_json as JSON;
+pub use polywrap_msgpack::extensions::generic_map::GenericMap as Map;
+pub use serde::{Deserialize,Serialize};
