7af0c6047b885ed92a43de8c89c551df13521e96
namesty <nestor09amesty@gmail.com>
2023-04-26 02:30:32 +0200

    (chore): uris in args instead of str refs

--- packages/native/src/builder.rs
+++ packages/native/src/builder.rs
@@ -1,6 +1,6 @@
 use polywrap_client::{
     builder::types::{BuilderConfig, ClientBuilder, ClientConfigHandler},
-    core::{package::WrapPackage, resolvers::uri_resolver_like::UriResolverLike}, client::PolywrapClient,
+    core::{package::WrapPackage, resolvers::uri_resolver_like::UriResolverLike, uri::Uri}, client::PolywrapClient,
 };
 use std::sync::{Arc, Mutex};
 
@@ -26,94 +26,94 @@ impl FFIBuilderConfig {
         }
     }
 
-    pub fn add_env(&self, uri: &str, env: &str) {
+    pub fn add_env(&self, uri: Arc<Uri>, env: &str) {
         self.inner_builder.lock().unwrap().add_env(
-            uri.to_string().try_into().unwrap(),
+            uri.as_ref().clone(),
             serde_json::from_str(env).unwrap(),
         );
     }
 
-    pub fn remove_env(&self, uri: &str) {
+    pub fn remove_env(&self, uri: Arc<Uri>) {
         self.inner_builder
             .lock()
             .unwrap()
-            .remove_env(uri.to_string().try_into().unwrap());
+            .remove_env(uri.as_ref().clone());
     }
 
-    pub fn set_env(&self, uri: &str, env: &str) {
+    pub fn set_env(&self, uri: Arc<Uri>, env: &str) {
         self.inner_builder.lock().unwrap().set_env(
-            uri.to_string().try_into().unwrap(),
+            uri.as_ref().clone(),
             serde_json::from_str(env).unwrap(),
         );
     }
 
-    pub fn add_interface_implementation(&self, interface_uri: &str, implementation_uri: &str) {
+    pub fn add_interface_implementation(&self, interface_uri: Arc<Uri>, implementation_uri: Arc<Uri>) {
         self.inner_builder
             .lock()
             .unwrap()
             .add_interface_implementation(
-                interface_uri.try_into().unwrap(),
-                implementation_uri.try_into().unwrap(),
+                interface_uri.as_ref().clone(),
+                implementation_uri.as_ref().clone(),
             );
     }
 
-    pub fn remove_interface_implementation(&self, interface_uri: &str, implementation_uri: &str) {
+    pub fn remove_interface_implementation(&self, interface_uri: Arc<Uri>, implementation_uri: Arc<Uri>) {
         self.inner_builder
             .lock()
             .unwrap()
             .remove_interface_implementation(
-                interface_uri.try_into().unwrap(),
-                implementation_uri.try_into().unwrap(),
+                interface_uri.as_ref().clone(),
+                implementation_uri.as_ref().clone()
             );
     }
 
-    pub fn add_wasm_wrapper(&self, uri: &str, wrapper: Arc<FFIWasmWrapper>) {
+    pub fn add_wasm_wrapper(&self, uri: Arc<Uri>, wrapper: Arc<FFIWasmWrapper>) {
         self.inner_builder.lock().unwrap().add_wrapper(
-            uri.to_string().try_into().unwrap(),
+            uri.as_ref().clone(),
             wrapper.inner_wasm_wrapper.clone(),
         );
     }
 
-    pub fn add_plugin_wrapper(&self, uri: &str, wrapper: Arc<FFIPluginWrapper>) {
+    pub fn add_plugin_wrapper(&self, uri: Arc<Uri>, wrapper: Arc<FFIPluginWrapper>) {
         self.inner_builder.lock().unwrap().add_wrapper(
-            uri.to_string().try_into().unwrap(),
+            uri.as_ref().clone(),
             wrapper.inner_plugin.clone(),
         );
     }
 
-    pub fn remove_wrapper(&self, uri: &str) {
+    pub fn remove_wrapper(&self, uri: Arc<Uri>) {
         self.inner_builder
             .lock()
             .unwrap()
-            .remove_wrapper(uri.try_into().unwrap());
+            .remove_wrapper(uri.as_ref().clone());
     }
 
-    pub fn add_package(&self, uri: &str, package: Box<dyn WrapPackage>) {
+    pub fn add_package(&self, uri: Arc<Uri>, package: Box<dyn WrapPackage>) {
         self.inner_builder
             .lock()
             .unwrap()
-            .add_package(uri.try_into().unwrap(), Arc::new(Mutex::new(package)));
+            .add_package(uri.as_ref().clone(), Arc::new(Mutex::new(package)));
     }
 
-    pub fn remove_package(&self, uri: &str) {
+    pub fn remove_package(&self, uri: Arc<Uri>) {
         self.inner_builder
             .lock()
             .unwrap()
-            .remove_package(uri.try_into().unwrap());
+            .remove_package(uri.as_ref().clone());
     }
 
-    pub fn add_redirect(&self, from: &str, to: &str) {
+    pub fn add_redirect(&self, from: Arc<Uri>, to: Arc<Uri>) {
         self.inner_builder.lock().unwrap().add_redirect(
-            from.to_string().try_into().unwrap(),
-            to.to_string().try_into().unwrap(),
+            from.as_ref().clone(),
+            to.as_ref().clone(),
         );
     }
 
-    pub fn remove_redirect(&self, from: &str) {
+    pub fn remove_redirect(&self, from: Arc<Uri>) {
         self.inner_builder
             .lock()
             .unwrap()
-            .remove_redirect(from.to_string().try_into().unwrap());
+            .remove_redirect(from.as_ref().clone());
     }
 
     pub fn add_resolver(&self, resolver: Box<dyn FFIUriResolver>) {
