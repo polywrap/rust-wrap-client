4282f1ec2d84d529974eb3a0e50931833ebfe166
Jure Bogunovic <bogunovij@gmail.com>
2023-05-15 20:20:10 +0200

    formatting

--- packages/client/src/subinvoker.rs
+++ packages/client/src/subinvoker.rs
@@ -1,13 +1,13 @@
 use std::sync::{RwLock, Arc};
 
 use polywrap_core::{
-  resolvers::uri_resolution_context::{UriResolutionContext, UriResolutionStep}, 
-  invoker::Invoker, env::Env, error::Error, uri::Uri, interface_implementation::InterfaceImplementations
+    resolvers::uri_resolution_context::{UriResolutionContext, UriResolutionStep}, 
+    invoker::Invoker, env::Env, error::Error, uri::Uri, interface_implementation::InterfaceImplementations
 };
 
 pub struct Subinvoker {
-  pub invoke_context: RwLock<UriResolutionContext>,
-  invoker: Arc<dyn Invoker>,
+    pub invoke_context: RwLock<UriResolutionContext>,
+    invoker: Arc<dyn Invoker>,
 }
 
 impl Subinvoker {
@@ -27,21 +27,21 @@ impl Subinvoker {
 }
 
 impl Invoker for Subinvoker {
-  fn invoke_raw(
-      &self,
-      uri: &Uri,
-      method: &str,
-      args: Option<&[u8]>,
-      env: Option<&Env>,
-      _: Option<&mut UriResolutionContext>,
-  ) -> Result<Vec<u8>, Error> {
-      let mut context = self.invoke_context.write().unwrap();
-      self.invoker.invoke_raw(uri, method, args, env, Some(&mut context))
-  }
-  fn get_implementations(&self, uri: &Uri) -> Result<Vec<Uri>, Error> {
-      self.invoker.get_implementations(uri)
-  }
-  fn get_interfaces(&self) -> Option<InterfaceImplementations> {
-      self.invoker.get_interfaces()
-  }
+    fn invoke_raw(
+        &self,
+        uri: &Uri,
+        method: &str,
+        args: Option<&[u8]>,
+        env: Option<&Env>,
+        _: Option<&mut UriResolutionContext>,
+    ) -> Result<Vec<u8>, Error> {
+        let mut context = self.invoke_context.write().unwrap();
+        self.invoker.invoke_raw(uri, method, args, env, Some(&mut context))
+    }
+    fn get_implementations(&self, uri: &Uri) -> Result<Vec<Uri>, Error> {
+        self.invoker.get_implementations(uri)
+    }
+    fn get_interfaces(&self) -> Option<InterfaceImplementations> {
+        self.invoker.get_interfaces()
+    }
 }
