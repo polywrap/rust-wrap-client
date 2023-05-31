ebe18489af7b558fedb48e1faec0e4954aa8bb6d
Cesar <cesarbrazon10@gmail.com>
2023-05-31 20:26:47 +0200

    chore: remove rust nightly & experimental traits

--- packages/builder/tests/test_builder.rs
+++ packages/builder/tests/test_builder.rs
@@ -1,14 +1,12 @@
-#![feature(closure_lifetime_binder)]
-#![feature(trait_upcasting)]
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::Arc};
 
 use polywrap_client_builder::types::{BuilderConfig, ClientBuilder};
-use polywrap_core::{
-    uri::Uri,
-    client::UriRedirect
-};
+use polywrap_core::{client::UriRedirect, package::WrapPackage, uri::Uri};
 use polywrap_msgpack::msgpack;
-use polywrap_tests_utils::helpers::{get_mock_package, MockPackage, MockWrapper, get_mock_wrapper};
+use polywrap_tests_utils::helpers::{
+    get_different_mock_package, get_different_mock_wrapper, get_mock_invoker, get_mock_package,
+    get_mock_wrapper,
+};
 
 #[test]
 fn test_env_methods() {
@@ -50,34 +48,39 @@ fn test_interface_implementation_methods() {
     assert!(builder.interfaces.is_none());
 
     builder.add_interface_implementations(
-        interface_uri.clone(), 
-        vec![implementation_a_uri.clone(), implementation_b_uri.clone()]
+        interface_uri.clone(),
+        vec![implementation_a_uri.clone(), implementation_b_uri.clone()],
     );
 
     let interfaces = builder.interfaces.clone().unwrap();
     let implementations = interfaces.get(&interface_uri.to_string()).unwrap();
     assert!(builder.interfaces.is_some());
-    assert_eq!(implementations, &vec![implementation_a_uri.clone(), implementation_b_uri.clone()]);
+    assert_eq!(
+        implementations,
+        &vec![implementation_a_uri.clone(), implementation_b_uri.clone()]
+    );
 
     let implementation_c_uri = Uri::new("wrap://ens/implementation-c.eth");
     builder.add_interface_implementation(interface_uri.clone(), implementation_c_uri.clone());
 
     let interfaces = builder.interfaces.clone().unwrap();
     let implementations = interfaces.get(&interface_uri.to_string()).unwrap();
-    assert_eq!(implementations, &vec![
-        implementation_a_uri.clone(), 
-        implementation_b_uri.clone(),
-        implementation_c_uri.clone()
-    ]);
+    assert_eq!(
+        implementations,
+        &vec![
+            implementation_a_uri.clone(),
+            implementation_b_uri.clone(),
+            implementation_c_uri.clone()
+        ]
+    );
 
     builder.remove_interface_implementation(&interface_uri, &implementation_b_uri);
     let interfaces = builder.interfaces.clone().unwrap();
     let implementations = interfaces.get(&interface_uri.to_string()).unwrap();
-    assert_eq!(implementations, &vec![
-        implementation_a_uri,
-        implementation_c_uri
-    ]);
-
+    assert_eq!(
+        implementations,
+        &vec![implementation_a_uri, implementation_c_uri]
+    );
 }
 
 #[test]
@@ -86,28 +89,43 @@ fn test_redirects() {
     assert!(builder.redirects.is_none());
 
     let redirects = vec![
-        UriRedirect{
-            from: "ens/c.eth".to_string().try_into().unwrap(), 
-            to: "ens/d.eth".to_string().try_into().unwrap()
+        UriRedirect {
+            from: "ens/c.eth".to_string().try_into().unwrap(),
+            to: "ens/d.eth".to_string().try_into().unwrap(),
         },
-        UriRedirect{
-            from: "ens/f.eth".to_string().try_into().unwrap(), 
-            to: "ens/g.eth".to_string().try_into().unwrap()
+        UriRedirect {
+            from: "ens/f.eth".to_string().try_into().unwrap(),
+            to: "ens/g.eth".to_string().try_into().unwrap(),
         },
     ];
     builder.add_redirects(redirects);
 
     assert!(builder.redirects.is_some());
     let builder_redirects = builder.redirects.unwrap();
-    assert_eq!(builder_redirects[0].from, "ens/c.eth".to_string().try_into().unwrap());
-    assert_eq!(builder_redirects[0].to, "ens/d.eth".to_string().try_into().unwrap());
-    assert_eq!(builder_redirects[1].from, "ens/f.eth".to_string().try_into().unwrap());
-    assert_eq!(builder_redirects[1].to, "ens/g.eth".to_string().try_into().unwrap());
+    assert_eq!(
+        builder_redirects[0].from,
+        "ens/c.eth".to_string().try_into().unwrap()
+    );
+    assert_eq!(
+        builder_redirects[0].to,
+        "ens/d.eth".to_string().try_into().unwrap()
+    );
+    assert_eq!(
+        builder_redirects[1].from,
+        "ens/f.eth".to_string().try_into().unwrap()
+    );
+    assert_eq!(
+        builder_redirects[1].to,
+        "ens/g.eth".to_string().try_into().unwrap()
+    );
 
     let mut builder = BuilderConfig::new(None);
     assert!(builder.redirects.is_none());
 
-    builder.add_redirect("ens/a.eth".to_string().try_into().unwrap(), "ens/b.eth".to_string().try_into().unwrap());
+    builder.add_redirect(
+        "ens/a.eth".to_string().try_into().unwrap(),
+        "ens/b.eth".to_string().try_into().unwrap(),
+    );
     assert!(builder.redirects.is_some());
 
     builder.remove_redirect(&"ens/a.eth".to_string().try_into().unwrap());
@@ -119,43 +137,40 @@ fn test_packages() {
     let mut builder = BuilderConfig::new(None);
     assert!(builder.packages.is_none());
 
-    let uri_package_a = (
-      String::from("wrap://package/a").try_into().unwrap(),
-      get_mock_package(Some(String::from("a")))
-    );
+    let uri_a: Uri = String::from("wrap://package/a").try_into().unwrap();
+    let uri_b: Uri = String::from("wrap://package/b").try_into().unwrap();
+
+    let uri_package_a = (uri_a, get_mock_package());
 
     let uri_package_b = (
-        String::from("wrap://package/b").try_into().unwrap(),
-        get_mock_package(Some(String::from("b")))
+        uri_b.clone(),
+        get_mock_package(),
     );
 
     let uri_package_c = (
         String::from("wrap://package/c").try_into().unwrap(),
-        get_mock_package(Some(String::from("c")))
+        get_mock_package(),
     );
 
     builder.add_packages(vec![uri_package_a, uri_package_b, uri_package_c]);
     assert!(builder.packages.is_some());
+
     let builder_packages = builder.packages.unwrap();
     assert_eq!(builder_packages.len(), 3);
 
-    {
-        let package_from_builder = &*(builder_packages[1].1) as &dyn std::any::Any;
-        let received_package = package_from_builder.downcast_ref::<MockPackage>().unwrap();
-        
-        let mock_package = get_mock_package(Some(String::from("b")));
-        let mock_package_as_any = &*(mock_package) as &dyn std::any::Any;
-        let expected_package = mock_package_as_any.downcast_ref::<MockPackage>().unwrap();
-        assert_eq!(received_package.name, expected_package.name);
-    }
+    let package: &Arc<dyn WrapPackage> = &builder_packages[1].1;
+    let wrapper = package.create_wrapper().unwrap();
+
+    let result_package_a = wrapper.invoke("", None, None, get_mock_invoker(), None);
+    assert_eq!(result_package_a.unwrap(), vec![2]);
 
     // We need to recreate the builder because when we do builder.packages.unwrap
     // the ownership is given, not allowing us to call the builder again
     let mut builder = BuilderConfig::new(None);
 
     let modified_uri_package_b = (
-        String::from("wrap://package/b").try_into().unwrap(),
-        get_mock_package(Some(String::from("b-modified")))
+        uri_b.clone(),
+        get_different_mock_package(),
     );
 
     builder.add_packages(builder_packages);
@@ -164,17 +179,14 @@ fn test_packages() {
 
     let builder_packages = builder.packages.unwrap();
     assert_eq!(builder_packages.len(), 2);
-
-    let b_package = builder_packages.into_iter().find(|(uri, _)| uri == &String::from("wrap://package/b").try_into().unwrap()).unwrap();
-    let package_from_builder = &*(b_package.1) as &dyn std::any::Any;
-    let received_package = package_from_builder.downcast_ref::<MockPackage>().unwrap();
-
-    let mock_package = get_mock_package(Some(String::from("b-modified")));
-    let mock_package_as_any = &*(mock_package) as &dyn std::any::Any;
-    let expected_package = mock_package_as_any.downcast_ref::<MockPackage>().unwrap();
-    assert_eq!(received_package.name, expected_package.name);
+    let b_package = builder_packages
+        .into_iter()
+        .find(|(uri, _)| uri == &uri_b)
+        .unwrap();
+    let wrapper = b_package.1.create_wrapper().unwrap();
+    let result_package_a = wrapper.invoke("", None, None, get_mock_invoker(), None);
+    assert_eq!(result_package_a.unwrap(), vec![1]);
 }
-
 
 #[test]
 fn test_wrappers() {
@@ -183,17 +195,17 @@ fn test_wrappers() {
 
     let uri_wrapper_a = (
         String::from("wrap://wrapper/a").try_into().unwrap(),
-        get_mock_wrapper(Some(String::from("a")))
+        get_mock_wrapper(),
     );
 
     let uri_wrapper_b = (
         String::from("wrap://wrapper/b").try_into().unwrap(),
-        get_mock_wrapper(Some(String::from("b")))
+        get_mock_wrapper(),
     );
 
     let uri_wrapper_c = (
         String::from("wrap://wrapper/c").try_into().unwrap(),
-        get_mock_wrapper(Some(String::from("c")))
+        get_mock_wrapper(),
     );
 
     builder.add_wrappers(vec![uri_wrapper_a, uri_wrapper_b, uri_wrapper_c]);
@@ -201,23 +213,13 @@ fn test_wrappers() {
     let builder_wrappers = builder.wrappers.unwrap();
     assert_eq!(builder_wrappers.len(), 3);
 
-    {
-        let wrapper_from_builder = &*(builder_wrappers[1].1) as &dyn std::any::Any;
-        let received_wrapper = wrapper_from_builder.downcast_ref::<MockWrapper>().unwrap();
-        
-        let mock_wrapper = get_mock_wrapper(Some(String::from("b")));
-        let mock_wrapper_as_any = &*(mock_wrapper) as &dyn std::any::Any;
-        let expected_wrapper = mock_wrapper_as_any.downcast_ref::<MockWrapper>().unwrap();
-        assert_eq!(received_wrapper.name, expected_wrapper.name);
-    }
-
     // We need to recreate the builder because when we do builder.wrappers.unwrap
     // the ownership is given, not allowing us to call the builder again
     let mut builder = BuilderConfig::new(None);
 
     let modified_uri_wrapper_b = (
         String::from("wrap://wrapper/b").try_into().unwrap(),
-        get_mock_wrapper(Some(String::from("b-modified")))
+        get_different_mock_wrapper(),
     );
 
     builder.add_wrappers(builder_wrappers);
@@ -227,12 +229,13 @@ fn test_wrappers() {
     let builder_wrappers = builder.wrappers.unwrap();
     assert_eq!(builder_wrappers.len(), 2);
 
-    let b_wrapper = builder_wrappers.into_iter().find(|(uri, _)| uri == &String::from("wrap://wrapper/b").try_into().unwrap()).unwrap();
-    let wrapper_from_builder = &*(b_wrapper.1) as &dyn std::any::Any;
-    let received_wrapper = wrapper_from_builder.downcast_ref::<MockWrapper>().unwrap();
+    let wrapper_uri = String::from("wrap://wrapper/b").try_into().unwrap();
+    let b_wrapper = builder_wrappers
+        .into_iter()
+        .find(|(uri, _)| uri == &wrapper_uri)
+        .unwrap();
 
-    let mock_wrapper = get_mock_wrapper(Some(String::from("b-modified")));
-    let mock_wrapper_as_any = &*(mock_wrapper) as &dyn std::any::Any;
-    let expected_wrapper = mock_wrapper_as_any.downcast_ref::<MockWrapper>().unwrap();
-    assert_eq!(received_wrapper.name, expected_wrapper.name);
+    // let result_wrapper_b = wrapper.1
+    let result_package_b = b_wrapper.1.invoke("", None, None, get_mock_invoker(), None);
+    assert_eq!(result_package_b.unwrap(), [1]);
 }
--- packages/client/tests/test_uri_resolvers.rs
+++ packages/client/tests/test_uri_resolvers.rs
@@ -1,4 +1,3 @@
-#![feature(trait_upcasting)]
 
 
 
