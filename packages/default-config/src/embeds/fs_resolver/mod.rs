fc5a663a62273ceb302e7d677940b9867fb3b08c
Jure Bogunovic <bogunovij@gmail.com>
2023-06-12 18:36:01 +0200

    using const bytes

--- packages/default-config/src/embeds/fs_resolver/mod.rs
+++ packages/default-config/src/embeds/fs_resolver/mod.rs
@@ -2,22 +2,17 @@ use polywrap_core::file_reader::SimpleFileReader;
 use polywrap_wasm::{wasm_package::WasmPackage, wasm_wrapper::WasmWrapper};
 use std::sync::Arc;
 
-fn wrap_wasm() -> Vec<u8> {
-    include_bytes!("./wrap.wasm").to_vec()
-}
-
-fn wrap_info() -> Vec<u8> {
-    include_bytes!("./wrap.info").to_vec()
-}
+const WRAP_INFO: &[u8] = include_bytes!("./wrap.info");
+const WRAP_WASM: &[u8] = include_bytes!("./wrap.wasm");
 
 pub fn wasm_package() -> WasmPackage {
     WasmPackage::new(
         Arc::new(SimpleFileReader::new()),
-        Some(wrap_info()),
-        Some(wrap_wasm()),
+        Some(WRAP_INFO.to_vec()),
+        Some(WRAP_WASM.to_vec()),
     )
 }
 
 pub fn wasm_wrapper() -> WasmWrapper {
-    WasmWrapper::new(wrap_wasm(), Arc::new(SimpleFileReader::new()))
+    WasmWrapper::new(WRAP_WASM.to_vec(), Arc::new(SimpleFileReader::new()))
 }
