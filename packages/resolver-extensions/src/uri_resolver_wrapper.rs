7486df7f363cf4c61d17e1954bb7530621d9325d
Jure Bogunovic <bogunovij@gmail.com>
2023-06-15 18:48:21 +0200

    added test for uri_resolver_wrapper and changed resolution to use both manifest and resolved uri

--- packages/resolver-extensions/src/uri_resolver_wrapper.rs
+++ packages/resolver-extensions/src/uri_resolver_wrapper.rs
@@ -89,25 +89,114 @@ impl UriResolver for UriResolverWrapper {
             invoker
         );
 
+        let uri = if let Some(resolved_uri) = result.uri {
+            Uri::try_from(resolved_uri)?
+        } else {
+            uri.clone()
+        };
+
         if let Some(manifest) = result.manifest {
             let package = WasmPackage::new(
                 Arc::new(file_reader),
                 Some(manifest),
                 None
             );
-            return Ok(UriPackageOrWrapper::Package(uri.clone(), Arc::new(package)));
+            return Ok(UriPackageOrWrapper::Package(
+                uri.clone(), 
+                Arc::new(package
+            )));
         }
 
-        if let Some(uri) = result.uri {
-            return Ok(UriPackageOrWrapper::Uri(uri.try_into()?));
-        }
-
-        Ok(UriPackageOrWrapper::Uri(uri.clone()))
+        Ok(UriPackageOrWrapper::Uri(uri))
     }
 }
 
 impl fmt::Debug for UriResolverWrapper {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "UriResolverWrapper: {:?}", self.implementation_uri)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::sync::{Arc, Mutex};
+
+    use polywrap_core::{uri::Uri, resolution::{uri_resolution_context::{UriResolutionContext, UriPackageOrWrapper}, uri_resolver::UriResolver}, invoker::Invoker, package::GetManifestOptions};
+    use polywrap_msgpack::rmp_serde::encode;
+
+    use super::{UriResolverWrapper, MaybeUriOrManifest};
+
+    struct MockInvoker {}
+
+    impl Invoker for MockInvoker {
+        fn invoke_raw(
+            &self,
+            _uri: &Uri,
+            _method_name: &str,
+            _args: Option<&[u8]>,
+            _env: Option<&[u8]>,
+            _invocation_context: Option<Arc<Mutex<UriResolutionContext>>>,
+        ) -> Result<Vec<u8>, polywrap_core::error::Error> {
+            let manifest = wrap_manifest_schemas::versions::WrapManifest01 {
+                abi: wrap_manifest_schemas::versions::WrapManifest01Abi {
+                    ..Default::default()
+                },
+                name: "mock".to_string(),
+                version: "0.1".to_string(),
+                type_: "wasm".to_string()
+            };
+            let manifest = encode::to_vec_named(&manifest).unwrap();
+
+            let result: Vec<u8> = encode::to_vec_named(&MaybeUriOrManifest {
+                uri: Some("wrap://mock/resolved-uri".to_string()),
+                manifest: Some(manifest)
+            }).unwrap();
+
+            Ok(result)
+        }
+
+        fn get_implementations(&self, _uri: &Uri) -> Result<Vec<Uri>, polywrap_core::error::Error> {
+            Ok(vec![])
+        }
+
+        fn get_interfaces(&self) -> Option<polywrap_core::interface_implementation::InterfaceImplementations> {
+            None
+        }
+
+        fn get_env_by_uri(&self, _uri: &Uri) -> Option<Vec<u8>> {
+            None
+        }
+    }
+
+    #[test]
+    fn sanity() {
+        let resolver_extension = UriResolverWrapper {
+            implementation_uri: Uri::try_from("wrap://mock/extension-uri").unwrap()
+        };
+
+        let result = resolver_extension.try_resolve_uri(
+            &Uri::try_from("wrap://mock/uri-to-resolver").unwrap(),
+            Arc::new(MockInvoker {}),
+            Arc::new(Mutex::new(UriResolutionContext::new()))
+        ).unwrap();
+        
+        let expected_manifest = wrap_manifest_schemas::versions::WrapManifest01 {
+            abi: wrap_manifest_schemas::versions::WrapManifest01Abi {
+                ..Default::default()
+            },
+            name: "mock".to_string(),
+            version: "0.1".to_string(),
+            type_: "wasm".to_string()
+        };
+
+        match result {
+            UriPackageOrWrapper::Package(uri, package) => {
+                assert_eq!(uri, Uri::try_from("wrap://mock/resolved-uri").unwrap());
+                assert_eq!(package.get_manifest(Some(&GetManifestOptions {
+                    no_validate: true
+                })).unwrap(), expected_manifest);
+            },
+            _ => panic!("Expected UriPackageOrWrapper::Package")
+        }
     }
 }
