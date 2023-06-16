dffab0aa5ac381b1c6d94d245d889548d6fba65f
Jure Bogunovic <bogunovij@gmail.com>
2023-06-16 20:35:26 +0200

    moved e2e tests to default config

---
+++ packages/default-config/tests/ipfs_tests.rs
@@ -0,0 +1,24 @@
+use polywrap_client::{client::PolywrapClient, builder::types::ClientConfigHandler};
+use polywrap_core::uri::Uri;
+use polywrap_msgpack::msgpack;
+
+const SUBINVOKE_WRAP_IPFS_URI: &str = "wrap://ipfs/Qmf7jukQhTQekdSgKfdnFtB6ERTN6V7aT4oYpzesDyr2cS";
+
+#[test]
+fn sanity() {
+    let config = polywrap_client_default_config::build();
+    let client = PolywrapClient::new(config.build());
+
+    let result = client.invoke::<u32>(
+        &Uri::try_from(SUBINVOKE_WRAP_IPFS_URI).unwrap(),
+        "add", 
+        Some(&msgpack!({
+            "a": 2,
+            "b": 40
+        })),
+        None,
+        None
+    ).unwrap();
+
+    assert_eq!(result, 42);
+}
