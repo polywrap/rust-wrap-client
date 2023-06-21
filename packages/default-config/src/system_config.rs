f27c1749cfe33cdc2e675a8c5ee9310c2814fbf7
Jure Bogunovic <bogunovij@gmail.com>
2023-06-21 16:28:26 +0200

    implemented newtype pattern for system and web3 configs

--- packages/default-config/src/system_config.rs
+++ packages/default-config/src/system_config.rs
@@ -9,67 +9,63 @@ use std::{collections::HashMap, sync::Arc};
 
 use crate::embeds::{fs_resolver, http_resolver};
 
-pub struct SystemClientConfig {
-    inner_config: PolywrapClientConfig,
-}
+pub struct SystemClientConfig(PolywrapClientConfig);
 
 impl Default for SystemClientConfig {
     fn default() -> Self {
-        Self {
-            inner_config: {
-                PolywrapClientConfig {
-                    redirects: Some(HashMap::from([
-                        (
-                            uri!("wrap://ens/wraps.eth:http@1.1.0"),
-                            uri!("plugin/http@1.1.0"),
-                        ),
-                        (
-                            uri!("wrap://ens/wraps.eth:file-system@1.0.0"),
-                            uri!("plugin/file-system@1.0.0"),
-                        ),
-                    ])),
-                    interfaces: Some(HashMap::from([(
-                        "wrap://ens/uri-resolver.core.polywrap.eth".to_string(),
-                        vec![
-                            uri!("ens/wraps.eth:http-uri-resolver-ext@1.0.1"),
-                            uri!("ens/wraps.eth:file-system-uri-resolver-ext@1.0.1"),
-                        ],
-                    )])),
-                    wrappers: Some(vec![
-                        (
-                            uri!("ens/wraps.eth:file-system-uri-resolver-ext@1.0.1"),
-                            Arc::new(fs_resolver::wasm_wrapper()),
-                        ),
-                        (
-                            uri!("ens/wraps.eth:http-uri-resolver-ext@1.0.1"),
-                            Arc::new(http_resolver::wasm_wrapper()),
-                        ),
-                    ]),
-                    packages: Some(vec![
-                        (
-                            uri!("plugin/file-system@1.0.0"),
-                            Arc::new(PluginPackage::from(FileSystemPlugin {})),
-                        ),
-                        (
-                            uri!("plugin/http@1.1.0"),
-                            Arc::new(PluginPackage::from(HttpPlugin {})),
-                        ),
-                    ]),
-                    ..Default::default()
-                }
-            },
-        }
+        Self({
+            PolywrapClientConfig {
+                redirects: Some(HashMap::from([
+                    (
+                        uri!("wrap://ens/wraps.eth:http@1.1.0"),
+                        uri!("plugin/http@1.1.0"),
+                    ),
+                    (
+                        uri!("wrap://ens/wraps.eth:file-system@1.0.0"),
+                        uri!("plugin/file-system@1.0.0"),
+                    ),
+                ])),
+                interfaces: Some(HashMap::from([(
+                    "wrap://ens/uri-resolver.core.polywrap.eth".to_string(),
+                    vec![
+                        uri!("ens/wraps.eth:http-uri-resolver-ext@1.0.1"),
+                        uri!("ens/wraps.eth:file-system-uri-resolver-ext@1.0.1"),
+                    ],
+                )])),
+                wrappers: Some(vec![
+                    (
+                        uri!("ens/wraps.eth:file-system-uri-resolver-ext@1.0.1"),
+                        Arc::new(fs_resolver::wasm_wrapper()),
+                    ),
+                    (
+                        uri!("ens/wraps.eth:http-uri-resolver-ext@1.0.1"),
+                        Arc::new(http_resolver::wasm_wrapper()),
+                    ),
+                ]),
+                packages: Some(vec![
+                    (
+                        uri!("plugin/file-system@1.0.0"),
+                        Arc::new(PluginPackage::from(FileSystemPlugin {})),
+                    ),
+                    (
+                        uri!("plugin/http@1.1.0"),
+                        Arc::new(PluginPackage::from(HttpPlugin {})),
+                    ),
+                ]),
+                ..Default::default()
+            }
+        })
     }
 }
 
 impl Into<PolywrapClientConfig> for SystemClientConfig {
     fn into(self) -> PolywrapClientConfig {
-        self.inner_config
+        self.0
     }
 }
 
 impl Into<ClientConfig> for SystemClientConfig {
     fn into(self) -> ClientConfig {
-        self.inner_config.into()
+        self.0.into()
     }
 }
--- packages/default-config/src/web3_config.rs
+++ packages/default-config/src/web3_config.rs
@@ -6,58 +6,52 @@ use std::{collections::HashMap, sync::Arc};
 
 use crate::embeds::{ipfs_http_client, ipfs_resolver};
 
-pub struct Web3ClientConfig {
-    inner_config: PolywrapClientConfig,
-}
+pub struct Web3ClientConfig(PolywrapClientConfig);
 
 impl Default for Web3ClientConfig {
     fn default() -> Self {
-        Self {
-            inner_config: {
-                PolywrapClientConfig {
-                    interfaces: Some(HashMap::from([
-                        (
-                            "wrap://ens/uri-resolver.core.polywrap.eth".to_string(),
-                            vec![uri!("ens/wraps.eth:async-ipfs-uri-resolver-ext@1.0.1")],
-                        ),
-                        (
-                            "wrap://ens/wraps.eth:ipfs-http-client@1.0.0".to_string(),
-                            vec![uri!("wrap://ens/wraps.eth:ipfs-http-client@1.0.0")],
-                        ),
-                    ])),
-                    envs: Some(HashMap::from([(
-                        uri!("wrap://ens/wraps.eth:async-ipfs-uri-resolver-ext@1.0.1"),
-                        msgpack!({
-                            "provider": "https://ipfs.wrappers.io",
-                            "fallbackProviders": ["https://ipfs.io"],
-                            "retries": { "tryResolveUri": 2, "getFile": 2 },
-                        }),
-                    )])),
-                    wrappers: Some(vec![
-                        (
-                            uri!("wrap://ens/wraps.eth:ipfs-http-client@1.0.0"),
-                            Arc::new(ipfs_http_client::wasm_wrapper()),
-                        ),
-                        (
-                            uri!("ens/wraps.eth:async-ipfs-uri-resolver-ext@1.0.1"),
-                            Arc::new(ipfs_resolver::wasm_wrapper()),
-                        ),
-                    ]),
-                    ..Default::default()
-                }
-            },
-        }
+        Self(PolywrapClientConfig {
+            interfaces: Some(HashMap::from([
+                (
+                    "wrap://ens/uri-resolver.core.polywrap.eth".to_string(),
+                    vec![uri!("ens/wraps.eth:async-ipfs-uri-resolver-ext@1.0.1")],
+                ),
+                (
+                    "wrap://ens/wraps.eth:ipfs-http-client@1.0.0".to_string(),
+                    vec![uri!("wrap://ens/wraps.eth:ipfs-http-client@1.0.0")],
+                ),
+            ])),
+            envs: Some(HashMap::from([(
+                uri!("wrap://ens/wraps.eth:async-ipfs-uri-resolver-ext@1.0.1"),
+                msgpack!({
+                    "provider": "https://ipfs.wrappers.io",
+                    "fallbackProviders": ["https://ipfs.io"],
+                    "retries": { "tryResolveUri": 2, "getFile": 2 },
+                }),
+            )])),
+            wrappers: Some(vec![
+                (
+                    uri!("wrap://ens/wraps.eth:ipfs-http-client@1.0.0"),
+                    Arc::new(ipfs_http_client::wasm_wrapper()),
+                ),
+                (
+                    uri!("ens/wraps.eth:async-ipfs-uri-resolver-ext@1.0.1"),
+                    Arc::new(ipfs_resolver::wasm_wrapper()),
+                ),
+            ]),
+            ..Default::default()
+        })
     }
 }
 
 impl Into<PolywrapClientConfig> for Web3ClientConfig {
     fn into(self) -> PolywrapClientConfig {
-        self.inner_config
+        self.0
     }
 }
 
 impl Into<ClientConfig> for Web3ClientConfig {
     fn into(self) -> ClientConfig {
-        self.inner_config.into()
+        self.0.into()
     }
 }
