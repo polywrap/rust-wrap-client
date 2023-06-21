1c7f0ed92aba372de6cfa87552048b5cec77eb82
Jure Bogunovic <bogunovij@gmail.com>
2023-06-21 21:29:28 +0200

    added deserialized module invocation test

--- packages/wasm/tests/test_runtime.rs
+++ packages/wasm/tests/test_runtime.rs
@@ -128,3 +128,39 @@ fn invoke_from_compiled_module() {
         .unwrap();
     assert_eq!(result, [2])
 }
+
+
+#[test]
+fn invoke_from_deserialized_module() {
+    let test_path = get_tests_path().unwrap();
+    let path = test_path.into_os_string().into_string().unwrap();
+
+    let module_path = format!("{path}/subinvoke/00-subinvoke/implementations/as/wrap.wasm");
+    let manifest_path = format!("{path}/subinvoke/00-subinvoke/implementations/as/wrap.info");
+
+    let module_bytes = fs::read(Path::new(&module_path)).unwrap();
+    let manifest_bytes = fs::read(Path::new(&manifest_path)).unwrap();
+
+    let _manifest = deserialize_wrap_manifest(&manifest_bytes, None).unwrap();
+    let file_reader = SimpleFileReader::new();
+
+    let compiled_module = CompiledWasmModule::try_from_bytecode(&module_bytes).unwrap();
+
+    let result = compiled_module.serialize().unwrap();
+
+    let module = result.deserialize().unwrap();
+
+    let wrapper = WasmWrapper::new(module, Arc::new(file_reader));
+
+    let mock_invoker = MockInvoker::new(wrapper);
+    let result = Arc::new(mock_invoker)
+        .invoke_raw(
+            &uri!("ens/wrapper.eth"),
+            "add",
+            Some(&msgpack!({ "a": 1, "b": 1})),
+            None,
+            None,
+        )
+        .unwrap();
+    assert_eq!(result, [2])
+}
