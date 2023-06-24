91a9eb5223db29cc3446e464886bd99744ba38b3
Jure Bogunovic <bogunovij@gmail.com>
2023-06-24 20:36:03 +0200

    formatting

--- packages/plugin/tests/e2e.rs
+++ packages/plugin/tests/e2e.rs
@@ -1,31 +1,26 @@
 use std::sync::Arc;
 
 use polywrap_client::client::PolywrapClient;
-use polywrap_client_builder::{PolywrapClientConfig, PolywrapClientConfigBuilder};
-use polywrap_core::{macros::uri, error::Error};
 use polywrap_client::core::uri::Uri;
-use polywrap_plugin::{package::PluginPackage, error::PluginError};
-use polywrap_tests_utils::mocks::{MemoryStoragePlugin, ArgsSetData};
+use polywrap_client_builder::{PolywrapClientConfig, PolywrapClientConfigBuilder};
+use polywrap_core::{error::Error, macros::uri};
+use polywrap_plugin::{error::PluginError, package::PluginPackage};
+use polywrap_tests_utils::mocks::{ArgsSetData, MemoryStoragePlugin};
 
 #[test]
 fn invoke_methods() {
     let plugin_uri = uri!("mock/plugin");
 
     let mut config = PolywrapClientConfig::new();
-    config.add_package(plugin_uri.clone(), Arc::new(PluginPackage::from(MemoryStoragePlugin {
-        value: 1
-    })));
+    config.add_package(
+        plugin_uri.clone(),
+        Arc::new(PluginPackage::from(MemoryStoragePlugin { value: 1 })),
+    );
 
     let client = PolywrapClient::new(config.into());
 
     let result = client
-        .invoke::<i32>(
-            &plugin_uri,
-            "getData",
-            None,
-            None,
-            None,
-        )
+        .invoke::<i32>(&plugin_uri, "getData", None, None, None)
         .unwrap();
     assert_eq!(result, 1);
 
@@ -38,19 +33,11 @@ fn invoke_methods() {
             None,
         )
         .unwrap();
-
     assert_eq!(result, true);
 
     let result = client
-        .invoke::<i32>(
-            &plugin_uri,
-            "getData",
-            None,
-            None,
-            None,
-        )
+        .invoke::<i32>(&plugin_uri, "getData", None, None, None)
         .unwrap();
-
     assert_eq!(result, 42);
 }
 
@@ -60,31 +47,27 @@ fn invoke_non_existent_method_should_err() {
     let method = String::from("iDontExist");
 
     let mut config = PolywrapClientConfig::new();
-    config.add_package(plugin_uri.clone(), Arc::new(PluginPackage::from(MemoryStoragePlugin {
-        value: 1
-    })));
+    config.add_package(
+        plugin_uri.clone(),
+        Arc::new(PluginPackage::from(MemoryStoragePlugin { value: 1 })),
+    );
 
     let client = PolywrapClient::new(config.into());
 
-    let result = client
-        .invoke::<i32>(
-            &plugin_uri,
-            &method,
-            None,
-            None,
-            None,
-        );
-    
+    let result = client.invoke::<i32>(&plugin_uri, &method, None, None, None);
+
     if let Err(err) = result {
         assert_eq!(
-            err.to_string(), 
+            err.to_string(),
             Error::InvokeError(
-                plugin_uri.to_string(), 
-                method.clone(), 
-                Error::from(PluginError::InvocationError{
+                plugin_uri.to_string(),
+                method.clone(),
+                Error::from(PluginError::InvocationError {
                     exception: PluginError::MethodNotFoundError(method).to_string()
-                }).to_string()
-            ).to_string()
+                })
+                .to_string()
+            )
+            .to_string()
         );
     } else {
         panic!("Expected error, got result: {:?}", result);
