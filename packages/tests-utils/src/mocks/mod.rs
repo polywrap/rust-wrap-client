4c1034328f0a2a2726dc8648d9783ad8a1461c96
Jure Bogunovic <bogunovij@gmail.com>
2023-06-26 17:20:29 +0200

    chore(plugin): method not found return error instead of panic (#146)
    * no longer panicking on wrap abort
    
    * returning error instead of using abort handler, removed the handler, more errors less unwraps
    
    * minor refactor
    
    * returning error instead of panicking when plugin method is not found
    
    * added tests for plugin invoke and method not found
    
    * formatting
    
    * test(client): move plugin test invocation to client crate
    
    ---------
    
    Co-authored-by: Cesar <cesarbrazon10@gmail.com>

--- packages/tests-utils/src/mocks/mod.rs
+++ packages/tests-utils/src/mocks/mod.rs
@@ -1,12 +1,12 @@
 pub mod client;
 pub mod invoker;
 pub mod package;
-pub mod plugin;
+pub mod plugins;
 pub mod resolver;
 pub mod wrapper;
 pub use client::*;
 pub use invoker::*;
 pub use package::*;
-pub use plugin::*;
+pub use plugins::*;
 pub use resolver::*;
 pub use wrapper::*;
