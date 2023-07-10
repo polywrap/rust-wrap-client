2dc7b69c2c3a5f01f92dfd4af8095d0f6dda2fc2
namesty <nestor09amesty@gmail.com>
2023-07-10 15:14:43 +0200

    added compiled and serialized wasm module interfaces

--- packages/native/src/wasm_wrapper.rs
+++ packages/native/src/wasm_wrapper.rs
@@ -1,19 +1,55 @@
 use std::sync::Arc;
 
 use polywrap_client::core::{file_reader::SimpleFileReader, wrapper::Wrapper};
-use polywrap_wasm::wasm_wrapper::WasmWrapper;
+use polywrap_wasm::{wasm_wrapper::WasmWrapper, wasm_module::{CompiledWasmModule, SerializedWasmModule}};
 
 use crate::{error::FFIError, invoker::FFIInvoker};
+
+pub fn ffi_wasm_wrapper_from_bytecode(
+  bytes: &[u8]
+) -> Result<FFIWasmWrapper, FFIError> {
+    let wasm_module = CompiledWasmModule::try_from_bytecode(bytes)?;
+
+    Ok(FFIWasmWrapper::new(
+      Arc::new(FFICompiledWasmModule(Arc::new(wasm_module)))
+    ))
+}
+
+pub fn ffi_compiled_wasm_module_from_bytecode(
+  bytes: &[u8]
+) -> Result<FFICompiledWasmModule, FFIError> {
+  Ok(FFICompiledWasmModule(Arc::new(CompiledWasmModule::try_from_bytecode(bytes)?)))
+}
+
+pub struct FFISerializedWasmModule(Arc<SerializedWasmModule>);
+
+impl FFISerializedWasmModule {
+  pub fn deserialize(&self) -> Result<Arc<FFICompiledWasmModule>, FFIError> {
+    let deserialized = self.0.deserialize()?;
+
+    Ok(Arc::new(FFICompiledWasmModule(Arc::new(deserialized))))
+  }
+}
+
+pub struct FFICompiledWasmModule(Arc<CompiledWasmModule>);
+
+impl FFICompiledWasmModule {
+    pub fn serialize(&self) -> Result<Arc<FFISerializedWasmModule>, FFIError> {
+      let serialized = self.0.serialize()?;
+
+      Ok(Arc::new(FFISerializedWasmModule(Arc::new(serialized))))
+    }
+}
 
 pub struct FFIWasmWrapper {
     pub inner_wasm_wrapper: Arc<dyn Wrapper>,
 }
 
 impl FFIWasmWrapper {
-    pub fn new(wasm_module: Vec<u8>) -> FFIWasmWrapper {
+    pub fn new(compiled_wasm_module: Arc<FFICompiledWasmModule>) -> FFIWasmWrapper {
+        let compiled_wasm_module = compiled_wasm_module.as_ref().0.as_ref().clone();
         let wasm_wrapper =
-            WasmWrapper::try_from_bytecode(&wasm_module, Arc::new(SimpleFileReader::new()))
-                .unwrap();
+            WasmWrapper::new(compiled_wasm_module, Arc::new(SimpleFileReader::new()));
         FFIWasmWrapper {
             inner_wasm_wrapper: Arc::new(wasm_wrapper),
         }
