a1319d31650333e289baa03d3b175563d6851108
Nestor Amesty <nestor09amesty@gmail.com>
2023-08-09 21:00:06 +0200

    feat(ffi): expose polywrap client config getters (#178)
    * Exposed polywrap client config getters
    
    * added tests for FFIBuilderConfig new methods
    
    * renamed eth uris to mock
    
    * added packages to test
    
    * added remove redirect test and builder.build test
    
    ---------
    
    Co-authored-by: nerfZael <bogunovij@gmail.com>

--- packages/native/src/builder.rs
+++ packages/native/src/builder.rs
@@ -1,4 +1,7 @@
-use std::sync::{Arc, Mutex};
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
 
 use polywrap_client::{
     builder::{PolywrapClientConfig, PolywrapClientConfigBuilder},
@@ -6,11 +9,14 @@ use polywrap_client::{
     core::uri::Uri,
 };
 use polywrap_client_default_config::{SystemClientConfig, Web3ClientConfig};
- 
+
 use crate::{
     client::FFIClient,
     package::FFIWrapPackage,
-    resolvers::ffi_resolver::FFIUriResolver,
+    resolvers::{
+        ffi_resolver::FFIUriResolver,
+        uri_package_or_wrapper::{FFIUriWrapPackage, FFIUriWrapper},
+    },
     uri::FFIUri,
     wrapper::FFIWrapper,
 };
@@ -22,11 +28,80 @@ impl FFIBuilderConfig {
         FFIBuilderConfig(Mutex::new(PolywrapClientConfig::new()))
     }
 
+    pub fn get_interfaces(&self) -> Option<HashMap<String, Vec<Arc<FFIUri>>>> {
+        let config = self.0.try_lock().unwrap();
+
+        config.interfaces.clone().map(|interfaces| {
+            interfaces
+                .into_iter()
+                .map(|(uri, value)| (uri.to_string(), value.into_iter().map(|uri| Arc::new(FFIUri(uri))).collect()))
+                .collect()
+        })
+    }
+
+    pub fn get_envs(&self) -> Option<HashMap<String, Vec<u8>>> {
+        let config = self.0.try_lock().unwrap();
+
+        config.envs.clone().map(|envs| {
+            envs.into_iter()
+                .map(|(uri, value)| (uri.to_string(), value))
+                .collect()
+        })
+    }
+
+    pub fn get_wrappers(&self) -> Option<Vec<FFIUriWrapper>> {
+        let config = self.0.try_lock().unwrap();
+
+        config.wrappers.clone().map(|wrappers| {
+            wrappers
+                .into_iter()
+                .map(|(uri, wrapper)| {
+                    FFIUriWrapper::new(Arc::new(FFIUri(uri)), Box::new(wrapper))
+                })
+                .collect()
+        })
+    }
+
+    pub fn get_packages(&self) -> Option<Vec<FFIUriWrapPackage>> {
+        let config = self.0.try_lock().unwrap();
+
+        config.packages.clone().map(|packages| {
+            packages
+                .into_iter()
+                .map(|(uri, package)| {
+                    FFIUriWrapPackage::new(
+                        Arc::new(FFIUri(uri)),
+                        Box::new(package),
+                    )
+                })
+                .collect()
+        })
+    }
+
+    pub fn get_redirects(&self) -> Option<HashMap<String, Arc<FFIUri>>> {
+        let config = self.0.try_lock().unwrap();
+
+        config.redirects.clone().map(|redirects| {
+            redirects
+                .into_iter()
+                .map(|redirect| (redirect.0.to_string(), Arc::new(FFIUri(redirect.1))))
+                .collect()
+        })
+    }
+
+    pub fn get_resolvers(&self) -> Option<Vec<Arc<FFIUriResolver>>> {
+        let config = self.0.try_lock().unwrap();
+
+        config.resolvers.clone().map(|resolvers| {
+            resolvers
+                .into_iter()
+                .map(|resolver| Arc::new(FFIUriResolver(Box::new(resolver))))
+                .collect()
+        })
+    }
+
     pub fn add_env(&self, uri: Arc<FFIUri>, env: Vec<u8>) {
-        self.0
-            .lock()
-            .unwrap()
-            .add_env(uri.0.clone(), env);
+        self.0.lock().unwrap().add_env(uri.0.clone(), env);
     }
 
     pub fn remove_env(&self, uri: Arc<FFIUri>) {
@@ -72,10 +147,7 @@ impl FFIBuilderConfig {
     }
 
     pub fn add_wrapper(&self, uri: Arc<FFIUri>, wrapper: Arc<FFIWrapper>) {
-        self.0
-            .lock()
-            .unwrap()
-            .add_wrapper(uri.0.clone(), wrapper);
+        self.0.lock().unwrap().add_wrapper(uri.0.clone(), wrapper);
     }
 
     pub fn remove_wrapper(&self, uri: Arc<FFIUri>) {
@@ -83,10 +155,7 @@ impl FFIBuilderConfig {
     }
 
     pub fn add_package(&self, uri: Arc<FFIUri>, package: Arc<FFIWrapPackage>) {
-        self.0
-            .lock()
-            .unwrap()
-            .add_package(uri.0.clone(), package);
+        self.0.lock().unwrap().add_package(uri.0.clone(), package);
     }
 
     pub fn remove_package(&self, uri: Arc<FFIUri>) {
@@ -105,10 +174,7 @@ impl FFIBuilderConfig {
     }
 
     pub fn add_resolver(&self, resolver: Arc<FFIUriResolver>) {
-        self.0
-            .lock()
-            .unwrap()
-            .add_resolver(resolver);
+        self.0.lock().unwrap().add_resolver(resolver);
     }
 
     pub fn add_system_defaults(&self) {
@@ -120,7 +186,7 @@ impl FFIBuilderConfig {
     }
 
     pub fn add_web3_defaults(&self) {
-      self.0
+        self.0
             .lock()
             .unwrap()
             .add(Web3ClientConfig::default().into());
@@ -144,7 +210,7 @@ mod test {
     };
     use serde::Serialize;
 
-    use crate::{package::FFIWrapPackage, uri::ffi_uri_from_string, wrapper::FFIWrapper};
+    use crate::{package::FFIWrapPackage, uri::{ffi_uri_from_string, FFIUri}, wrapper::FFIWrapper};
 
     use super::FFIBuilderConfig;
 
@@ -172,6 +238,12 @@ mod test {
         let envs = builder.0.lock().unwrap().clone().envs.unwrap();
         let current_env = envs.get(&uri!("wrap://mock/some"));
         assert_eq!(&env, current_env.unwrap());
+        assert_eq!(builder.get_envs().unwrap(), HashMap::from([
+            (
+                "wrap://mock/some".to_string(),
+                env.clone()
+            )
+        ]));
 
         let new_env = to_vec(&AnotherArgs {
             bar: "foo".to_string(),
@@ -182,10 +254,17 @@ mod test {
         let envs = builder.0.lock().unwrap().clone().envs.unwrap();
         let current_env = envs.get(&uri!("wrap://mock/some"));
         assert_eq!(&new_env, current_env.unwrap());
+        assert_eq!(builder.get_envs().unwrap(), HashMap::from([
+            (
+                "wrap://mock/some".to_string(),
+                new_env.clone()
+            )
+        ]));
 
         builder.remove_env(uri);
         let envs = builder.0.lock().unwrap().clone().envs;
         assert_eq!(None, envs);
+        assert_eq!(builder.get_envs(), None);
     }
 
     #[test]
@@ -199,30 +278,12 @@ mod test {
 
         builder.add_package(uri_mock_package.clone(), mock_package);
         builder.add_package(uri_different_mock_package, different_mock_package);
-        assert_eq!(
-            builder
-                .0
-                .lock()
-                .unwrap()
-                .clone()
-                .packages
-                .unwrap()
-                .len(),
-            2
-        );
+        assert_eq!(builder.0.lock().unwrap().clone().packages.unwrap().len(), 2);
+        assert_eq!(builder.get_packages().unwrap().len(), 2);
 
         builder.remove_package(uri_mock_package);
-        assert_eq!(
-            builder
-                .0
-                .lock()
-                .unwrap()
-                .clone()
-                .packages
-                .unwrap()
-                .len(),
-            1
-        );
+        assert_eq!(builder.0.lock().unwrap().clone().packages.unwrap().len(), 1);
+        assert_eq!(builder.get_packages().unwrap().len(), 1);
     }
 
     #[test]
@@ -235,30 +296,12 @@ mod test {
 
         builder.add_wrapper(uri_mock_wrapper.clone().unwrap(), mock_package);
         builder.add_wrapper(uri_different_mock_wrapper.unwrap(), different_mock_wrapper);
-        assert_eq!(
-            builder
-                .0
-                .lock()
-                .unwrap()
-                .clone()
-                .wrappers
-                .unwrap()
-                .len(),
-            2
-        );
+        assert_eq!(builder.0.lock().unwrap().clone().wrappers.unwrap().len(), 2);
+        assert_eq!(builder.get_wrappers().unwrap().len(), 2);
 
         builder.remove_wrapper(uri_mock_wrapper.unwrap());
-        assert_eq!(
-            builder
-                .0
-                .lock()
-                .unwrap()
-                .clone()
-                .wrappers
-                .unwrap()
-                .len(),
-            1
-        );
+        assert_eq!(builder.0.lock().unwrap().clone().wrappers.unwrap().len(), 1);
+        assert_eq!(builder.get_wrappers().unwrap().len(), 1);
     }
 
     #[test]
@@ -273,13 +316,7 @@ mod test {
             ffi_uri_from_string("wrap/d").unwrap(),
         );
 
-        let redirects = builder
-            .0
-            .lock()
-            .unwrap()
-            .clone()
-            .redirects
-            .unwrap();
+        let redirects = builder.0.lock().unwrap().clone().redirects.unwrap();
         assert_eq!(
             redirects,
             HashMap::from([
@@ -287,6 +324,25 @@ mod test {
                 (uri!("wrap/c"), uri!("wrap/d")),
             ])
         );
+        assert_eq!(builder.get_redirects().unwrap(), HashMap::from([
+            ("wrap://wrap/a".to_string(), Arc::new(FFIUri(uri!("wrap/b")))),
+            ("wrap://wrap/c".to_string(), Arc::new(FFIUri(uri!("wrap/d")))),
+        ]));
+
+        builder.remove_redirect(
+            ffi_uri_from_string("wrap/a").unwrap(),
+        );
+
+        let redirects = builder.0.lock().unwrap().clone().redirects.unwrap();
+        assert_eq!(
+            redirects,
+            HashMap::from([
+                (uri!("wrap/c"), uri!("wrap/d")),
+            ])
+        );
+        assert_eq!(builder.get_redirects().unwrap(), HashMap::from([
+            ("wrap://wrap/c".to_string(), Arc::new(FFIUri(uri!("wrap/d")))),
+        ]));
     }
 
     #[test]
@@ -319,6 +375,19 @@ mod test {
                 uri!("wrap://mock/implementation-c")
             ])
         );
+        assert_eq!(
+            builder.get_interfaces(),
+            Some(HashMap::from([
+                (
+                    "wrap://mock/interface".to_string(),
+                    vec![
+                        Arc::new(FFIUri(uri!("wrap://mock/implementation-a"))),
+                        Arc::new(FFIUri(uri!("wrap://mock/implementation-b"))),
+                        Arc::new(FFIUri(uri!("wrap://mock/implementation-c")))
+                    ]
+                )
+            ]))
+        );
 
         builder.remove_interface_implementation(interface_uri.clone(), implementation_b_uri);
         let interfaces = builder
@@ -336,6 +405,18 @@ mod test {
                 uri!("wrap://mock/implementation-c")
             ])
         );
+        assert_eq!(
+            builder.get_interfaces(),
+            Some(HashMap::from([
+                (
+                    "wrap://mock/interface".to_string(),
+                    vec![
+                        Arc::new(FFIUri(uri!("wrap://mock/implementation-a"))),
+                        Arc::new(FFIUri(uri!("wrap://mock/implementation-c")))
+                    ]
+                )
+            ]))
+        );
     }
 
     #[test]
@@ -347,6 +428,8 @@ mod test {
         assert!(builder.0.lock().unwrap().interfaces.is_some());
         assert!(builder.0.lock().unwrap().wrappers.is_some());
         assert!(builder.0.lock().unwrap().packages.is_some());
+    
+        let _ = builder.build();
     }
 
     #[test]
@@ -356,5 +439,7 @@ mod test {
         assert!(builder.0.lock().unwrap().redirects.is_some());
         assert!(builder.0.lock().unwrap().interfaces.is_some());
         assert!(builder.0.lock().unwrap().packages.is_some());
+
+        let _ = builder.build();
     }
 }
