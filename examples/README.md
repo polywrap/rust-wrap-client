013a279bd4512f54bb3d1a59a4e56296b6810d28
Cesar <cesarbrazon10@gmail.com>
2023-08-14 13:20:15 +0200

    chore(examples): update structure and hello world

--- examples/README.md
+++ examples/README.md
@@ -1,5 +1,15 @@
-1. Hello world
+# Polywrap Rust Client Examples
+
+## Hello world
+
+Invokes the logger wrap, which interacts with the logger plugin. It shows a println message from WASM world
 
 ```shell
 cargo run --example hello-world --release
+```
+
+2. Ethereum
+
+```shell
+cargo run --example ethereum --release
 ```
--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -1,8 +1,21 @@
+extern crate polywrap_client;
+extern crate polywrap_client_builder;
+extern crate polywrap_client_default_config;
+extern crate polywrap_core;
+extern crate polywrap_logger_plugin;
+extern crate polywrap_msgpack_serde;
+extern crate polywrap_plugin;
+extern crate serde;
+
+use std::sync::Arc;
+
 use polywrap_client::client::PolywrapClient;
 use polywrap_client_builder::{PolywrapClientConfig, PolywrapClientConfigBuilder};
 use polywrap_client_default_config::SystemClientConfig;
 use polywrap_core::{client::ClientConfigBuilder, error::Error, macros::uri, uri::Uri};
+use polywrap_logger_plugin::LoggerPlugin;
 use polywrap_msgpack_serde::to_vec;
+use polywrap_plugin::package::PluginPackage;
 use serde::Serialize;
 
 #[derive(Serialize)]
@@ -11,17 +24,32 @@ struct LogMessageArgs {
 }
 
 fn main() {
+    let wrap_uri = uri!("wrapscan.io/polywrap/logging@1.0.0");
     let mut config = PolywrapClientConfig::new();
-    config.add(SystemClientConfig::default().into());
 
-    let uri = uri!("ipfs/Qmd3B3UPXoJYCWMjdnKa7Hs8SXpxLo2tQJfMdqpECbki7J");
+    let logger_plugin = LoggerPlugin::new(None);
+    let logger_package = PluginPackage::from(logger_plugin);
+
+    config.add_package(
+        uri!("wrapscan.io/polywrap/logger@1.0"),
+        Arc::new(logger_package),
+    );
+    config.add_interface_implementation(
+        uri!("wrapscan.io/polywrap/logger@1.0"),
+        uri!("wrapscan.io/polywrap/logger@1.0"),
+    );
+    println!("hai");
+    config.add(SystemClientConfig::default().into());
     let client = PolywrapClient::new(config.build());
     let result: Result<bool, Error> = client.invoke(
-        &uri,
-        "logMessage",
-        Some(&to_vec(&LogMessageArgs {
-            message: "hey".to_string(),
-        }).unwrap()),
+        &wrap_uri,
+        "info",
+        Some(
+            &to_vec(&LogMessageArgs {
+                message: "Hello from hello world wrap!".to_string(),
+            })
+            .unwrap(),
+        ),
         None,
         None,
     );
