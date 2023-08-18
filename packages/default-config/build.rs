b5388ceb0af855246039a3388162c3e169c6f065
nerfZael <bogunovij@gmail.com>
2023-08-18 20:56:01 +0200

    running build script only if wasm changes

--- packages/default-config/build.rs
+++ packages/default-config/build.rs
@@ -9,12 +9,13 @@ fn main() {
         if directory.file_type().unwrap().is_file() {
             continue;
         }
-
         let wasm = fs::read(format!("{}/wrap.wasm", directory.path().to_str().unwrap())).unwrap();
 
         let compiled_module = WasmModule::WasmBytecode(wasm.into()).compile().unwrap();
         let serialized_module = compiled_module.serialize().unwrap();
 
         fs::write(format!("{}/wrap.serialized", directory.path().to_str().unwrap()), serialized_module.serialize_for_storage()).unwrap();
+
+        println!("cargo:rerun-if-changed={}", format!("{}/wrap.wasm", directory.path().to_str().unwrap()));
     }
 }
