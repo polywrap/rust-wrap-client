2e141aa01d16523db39b0214cd76ce62a109296b
cbrzn <cesarbrazon10@gmail.com>
2023-11-23 14:14:21 +0000

    Deploying to gh-pages-reference from @ polywrap/rust-client@55916ff400b54c18a7ceda0c9dcbc606408ce126 🚀

--- implementors/polywrap_core/wrapper/trait.Wrapper.js
+++ implementors/polywrap_core/wrapper/trait.Wrapper.js
@@ -2,5 +2,5 @@
 "polywrap":[],
 "polywrap_native":[["impl <a class=\"trait\" href=\"polywrap_core/wrapper/trait.Wrapper.html\" title=\"trait polywrap_core::wrapper::Wrapper\">Wrapper</a> for <a class=\"struct\" href=\"polywrap_native/wrapper/struct.FFIWrapper.html\" title=\"struct polywrap_native::wrapper::FFIWrapper\">FFIWrapper</a>"]],
 "polywrap_plugin":[["impl&lt;T: <a class=\"trait\" href=\"polywrap_plugin/module/trait.PluginModule.html\" title=\"trait polywrap_plugin::module::PluginModule\">PluginModule</a> + 'static&gt; Wrapper for <a class=\"struct\" href=\"polywrap_plugin/wrapper/struct.PluginWrapper.html\" title=\"struct polywrap_plugin::wrapper::PluginWrapper\">PluginWrapper</a>&lt;T&gt;"]],
-"polywrap_wasm":[["impl Wrapper for <a class=\"struct\" href=\"polywrap_wasm/wasm_wrapper/struct.WasmWrapper.html\" title=\"struct polywrap_wasm::wasm_wrapper::WasmWrapper\">WasmWrapper</a>"]]
+"polywrap_wasm":[["impl <a class=\"trait\" href=\"polywrap_core/wrapper/trait.Wrapper.html\" title=\"trait polywrap_core::wrapper::Wrapper\">Wrapper</a> for <a class=\"struct\" href=\"polywrap_wasm/wasm_wrapper/struct.WasmWrapper.html\" title=\"struct polywrap_wasm::wasm_wrapper::WasmWrapper\">WasmWrapper</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
