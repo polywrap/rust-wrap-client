e3b642fce13400163a67c452ca63a795ea78fb95
Jure Granić Skender <3357638+pileks@users.noreply.github.com>
2023-11-23 14:50:42 +0100

    chore: re-export Client and ClientConfig with Polywrap prefixes (#248)

--- packages/builder/src/lib.rs
+++ packages/builder/src/lib.rs
@@ -5,3 +5,6 @@ pub mod polywrap_client_config_builder;
 pub use polywrap_base_resolver::{PolywrapBaseResolver, PolywrapBaseResolverOptions};
 pub use polywrap_client_config::ClientConfig;
 pub use polywrap_client_config_builder::ClientConfigBuilder;
+
+pub use polywrap_client_config_builder::ClientConfigBuilder as PolywrapClientConfigBuilder;
+pub use polywrap_client_config::ClientConfig as PolywrapClientConfig;
